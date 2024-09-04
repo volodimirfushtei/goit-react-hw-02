62416d25946bb1d9d72b3db3bf9f6f0dc33b373a
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-09-04 23:53:20 +0300

    add lock

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,49 @@ import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
+
+const LOCAL_STORAGE_KEY = "feedbacks";
 export default function App() {
   // Ініціалізуємо стан з об'єктом для типів відгуків
-  const [feedbacks, setFeedbacks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    Positive: 0,
+  const [feedbacks, setFeedbacks] = useState(() => {
+    const savedFeedbacks = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedFeedbacks
+      ? JSON.parse(savedFeedbacks)
+      : { good: 0, neutral: 0, bad: 0, total: 0, Positive: 0 };
   });
 
   const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
   {
     totalFeedback > 0 ? <Feedback /> : <Notification />;
   }
+
   const updateFeedback = (feedbackType) => {
-    setFeedbacks((prevFeedbacks) => ({
-      ...prevFeedbacks,
-      [feedbackType]: prevFeedbacks[feedbackType] + 1,
-    }));
+    setFeedbacks((prevFeedbacks) => {
+      const updatedFeedbacks = {
+        ...prevFeedbacks,
+        [feedbackType]: prevFeedbacks[feedbackType] + 1,
+      };
+      window.localStorage.setItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify(updatedFeedbacks)
+      );
+      return updatedFeedbacks;
+    });
   };
 
   const resetFeedback = () => {
-    setFeedbacks({
+    const resetFeedbacks = {
       good: 0,
       neutral: 0,
       bad: 0,
       total: 0,
       Positive: 0,
-    });
+    };
+    setFeedbacks(resetFeedbacks);
+    window.localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify(resetFeedbacks)
+    );
   };
 
   return (
